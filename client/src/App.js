2352d0798aa489c2ee6b7022b9aca1219c66c494
mainak <heellomeelo@gmail.com>
2023-05-21 11:28:04 +0530

    c

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("https://chatlipication.vercel.app")
+const socket = socketIO.connect("https://server-beta-ruby.vercel.app/")
 function App() {
   return (
     <BrowserRouter>
---
+++ server/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwaZCHK0OyCTeuSRTuntHb_FX-ZMzrz1A",
+  authDomain: "chat-48a5c.firebaseapp.com",
+  projectId: "chat-48a5c",
+  storageBucket: "chat-48a5c.appspot.com",
+  messagingSenderId: "576253076082",
+  appId: "1:576253076082:web:389cb96d268997f5cb58eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
