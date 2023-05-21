0c35bd10fe8b80d181bbb833462b65f217150a6d
mainak <heellomeelo@gmail.com>
2023-05-21 08:12:41 +0530

    c

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("https://chatlipication-9ufl.vercel.app")
+const socket = socketIO.connect("https://chatlipication.vercel.app")
 function App() {
   return (
     <BrowserRouter>
